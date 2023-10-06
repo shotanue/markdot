93b8894c5ce9c18c41ea96c3469edf9c8fd9d7b3
shotanue <shotanue@gmail.com>
2023-10-06 13:09:57 +0900

    fix: getEnv not defined

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,43 @@ type Tasks = {
   list: Task[];
 };
 
+const getEnv = (env?: Record<string, string | string[]>): Record<string, string> => {
+  const replaceEnvVars = (str: string, env: Record<string, string | undefined>): string => {
+    return str.replace(/\$([A-Z_][A-Z_0-9]*)/g, (_, varName) => env[varName] || `$${varName}`);
+  };
+
+  const mergeEnv = (
+    a: Record<string, string | undefined>,
+    b: Record<string, string | string[]>,
+  ): Record<string, string> => {
+    const result: Record<string, string> = {};
+
+    // Combine keys from both records
+    const allKeys = new Set([...Object.keys(a), ...Object.keys(b)]);
+
+    for (const key of allKeys) {
+      if (a[key] && b[key]) {
+        const valueFromB = Array.isArray(b[key])
+          ? (b[key] as string[]).map((item) => replaceEnvVars(item, a)).join(":")
+          : replaceEnvVars(b[key] as string, a);
+
+        result[key] = `${a[key]}:${valueFromB}`;
+      } else if (a[key]) {
+        result[key] = a[key] ?? "";
+      } else {
+        const valueFromB = Array.isArray(b[key])
+          ? (b[key] as string[]).map((item) => replaceEnvVars(item, a)).join(":")
+          : replaceEnvVars(b[key] as string, a);
+
+        result[key] = valueFromB;
+      }
+    }
+    return result;
+  };
+
+  return mergeEnv({ ...process.env }, { ...env });
+};
+
 type Log = (message: string, options?: { label: boolean }) => void;
 export type Ctx = {
   logger: {
@@ -205,40 +242,3 @@ try {
   }
   process.exit(1);
 }
-
-const getEnv = (env?: Record<string, string | string[]>): Record<string, string> => {
-  const replaceEnvVars = (str: string, env: Record<string, string | undefined>): string => {
-    return str.replace(/\$([A-Z_][A-Z_0-9]*)/g, (_, varName) => env[varName] || `$${varName}`);
-  };
-
-  const mergeEnv = (
-    a: Record<string, string | undefined>,
-    b: Record<string, string | string[]>,
-  ): Record<string, string> => {
-    const result: Record<string, string> = {};
-
-    // Combine keys from both records
-    const allKeys = new Set([...Object.keys(a), ...Object.keys(b)]);
-
-    for (const key of allKeys) {
-      if (a[key] && b[key]) {
-        const valueFromB = Array.isArray(b[key])
-          ? (b[key] as string[]).map((item) => replaceEnvVars(item, a)).join(":")
-          : replaceEnvVars(b[key] as string, a);
-
-        result[key] = `${a[key]}:${valueFromB}`;
-      } else if (a[key]) {
-        result[key] = a[key] ?? "";
-      } else {
-        const valueFromB = Array.isArray(b[key])
-          ? (b[key] as string[]).map((item) => replaceEnvVars(item, a)).join(":")
-          : replaceEnvVars(b[key] as string, a);
-
-        result[key] = valueFromB;
-      }
-    }
-    return result;
-  };
-
-  return mergeEnv({ ...process.env }, { ...env });
-};
